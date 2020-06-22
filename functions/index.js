1dc7fbf70ce9eeb315fb309e1fa58b93d1e25b87
Wichaelian <michael9wang@gmail.com>
2020-06-21 23:20:47 -0700

    stats database, expired bug patch

--- functions/index.js
+++ functions/index.js
@@ -36,14 +36,32 @@ exports.newDonor = functions.firestore.document('donors/{donorId}').onCreate( as
     });
 });
 
+
+
 //New matcher "submit" confirmation email
 exports.newMatcher = functions.firestore.document('matchers/{matcherId}').onCreate( async (change, context) => {
     const matcherSnap = await db.collection('matchers').doc(context.params.matcherId).get();
     const matcher = matcherSnap.data();
     const donorSnap = await db.collection('donors').doc(matcher.donorID).get();
     const donor = donorSnap.data();
+    const moneySnap = await db.collection('stats').doc('money').get();
+    const currentMoney = moneySnap.data();
+    const tSnap = await db.collection('stats').doc('traffic').get();
+    const traffic = tSnap.data();
+    const newTraffic = traffic.donations_matched + 1
+    const newDonated = currentMoney.donated + donor.amount
+    const newMatched = currentMoney.matched + (donor.amount * matcher.rate)
+    const newTotal = newDonated + newMatched 
     db.doc('donors/' + matcher.donorID).update({
         status: 'Matched by '+ context.params.matcherId
+    })
+    db.doc('stats/money').update({
+        donated: newDonated,
+        matched: newMatched,
+        total: newTotal
+    })
+    db.doc('stats/traffic').update({
+        donations_matched: newTraffic
     })
     const msg = {
         to: matcher.pemail,
@@ -92,20 +110,21 @@ exports.expired = functions.firestore.document('matchers/{matcherId}').onCreate(
                 db.doc('donors/' + doc.id).update({
                     status: 'expired'
                 });
+                const msg = {
+                    to: doc.data().email,
+                    from: 'Team BonoBud <teambonobud@gmail.com>',
+                    subject: doc.data().name + ', your donation listing has expired.',
+                    text: 'Unfortunately, we were unable to match you with a BonoBud',
+                };        
+                return transporter.sendMail(msg, (error, data) => {
+                    if (error) {
+                        console.log(error)
+                        return
+                    }
+                    console.log("Sent")
+                })
             }
-            const msg = {
-                to: doc.data().email,
-                from: 'Team BonoBud <teambonobud@gmail.com>',
-                subject: doc.data().name + ', your donation listing has expired.',
-                text: 'Unfortunately, we were unable to match you with a BonoBud',
-            };        
-            return transporter.sendMail(msg, (error, data) => {
-                if (error) {
-                    console.log(error)
-                    return
-                }
-                console.log("Sent")
-            }); 
+            return
         });
         return
     })

48c8c70be93b93e35a6fc4d8ffcd561b0bfe539b
rry6 <rry6@cornell.edu>
2020-06-25 14:51:19 -0500

    Added form validation

--- donor.js
+++ donor.js
@@ -39,31 +39,36 @@ const dsave = document.querySelector("#submitButton");
 
 //create new donor doc in firebase from input information
 dsave.addEventListener("click", function(){
-  var newdonor = db.collection("donors").doc();
-	var reason = dreason.value;
-	if (reason.length === 0) {
-		reason = "I really liked their mission!"; //sets a default message for reason
+	if(!empty()) {
+		var newdonor = db.collection("donors").doc();
+		var reason = dreason.value;
+		if (reason.length === 0) {
+			reason = "I really liked their mission!"; //sets a default message for reason
+		}
+		var first = dname.value.split(" ")[0]
+		newdonor.set({
+			name: dname.value,
+			firstname: first,
+			email: demail.value,
+			charity: dcharity.value,
+			charityArray: arrayify(dcharity.value),
+			link: dlink.value,
+			reason: reason,
+			amount: Number(damount.value),
+			date: firebase.firestore.FieldValue.serverTimestamp(),
+			status: "available" //available = display in feed, matcherid = donor is matched, expired = after 3 weeks
+		})
+			.then(function () {
+				location.href = 'submission.html'; //donor success page
+			})
+			.catch(function (error) {
+				console.error("Error adding donor: ", error);
+				location.href = 'submissionFail.html'; //submission failed page
+			});
 	}
-	var first = dname.value.split(" ")[0]
-  newdonor.set({
-    name: dname.value,
-		firstname: first,
-    email: demail.value,
-    charity: dcharity.value,
-		charityArray: arrayify(dcharity.value),
-    link: dlink.value,
-    reason: reason,
-    amount: Number(damount.value),
-    date: firebase.firestore.FieldValue.serverTimestamp(),
-		status: "available" //available = display in feed, matcherid = donor is matched, expired = after 3 weeks
-  })
-  .then(function() {
-      location.href='submission.html'; //donor success page
-  })
-  .catch(function(error) {
-      console.error("Error adding donor: ", error);
-      location.href='submissionFail.html'; //submission failed page
-  });
+	else{
+		return;
+	}
 })
 
 //turns a charity name into an array of all sequential word combinations for search functionality
@@ -86,3 +91,82 @@ function arrayify(phrase) {
 	}
 	return array;
 }
+
+function empty(){
+	var nameInput = document.getElementById("nameInput");
+	var emailInput = document.getElementById("emailInput");
+	var orgInput = document.getElementById("orgInput");
+	var linkInput = document.getElementById("linkInput");
+	var amount = document.getElementById("amount");
+	var isEmpty = false;
+	var alertString = " ";
+	var emailRegex = RegExp('^\\S+@\\S+$');
+	var linkRegex = RegExp('https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,}')
+
+	var isInvalid = false;
+
+	//Test name value
+	if(nameInput.value.trim() == ""){
+		nameInput.style.boxShadow = "rgb(255,105,97) 0px 1px";
+		isEmpty = true;
+	}
+	else{
+		nameInput.style.boxShadow = "rgb(181, 234, 215) 0px 1px";
+	}
+
+	//Test email value and validation
+	if(emailInput.value.trim() == ""){
+		emailInput.style.boxShadow = "rgb(255,105,97) 0px 1px";
+		isEmpty = true;
+	}
+	else if(!emailRegex.test(emailInput.value)){
+		emailInput.style.boxShadow = "rgb(255,105,97) 0px 1px";
+		alertString = alertString.concat(" ","Please enter a valid email.");
+		isInvalid = true;
+	}
+	else{
+		emailInput.style.boxShadow = "rgb(181, 234, 215) 0px 1px";
+	}
+
+	//Test org input
+	if(orgInput.value.trim() == ""){
+		orgInput.style.boxShadow = "rgb(255,105,97) 0px 1px";
+		isEmpty = true;
+	}
+	else{
+		orgInput.style.boxShadow = "rgb(181, 234, 215) 0px 1px";
+	}
+
+	//Test link input and validation
+	if(linkInput.value.trim() == ""){
+		linkInput.style.boxShadow = "rgb(255,105,97) 0px 1px";
+		isEmpty = true;
+	}
+	else if(!linkRegex.test(linkInput.value)){
+		linkInput.style.boxShadow = "rgb(255,105,97) 0px 1px";
+		alertString = alertString.concat(" ","Please enter a valid link.");
+		isInvalid = true;
+	}
+	else{
+		linkInput.style.boxShadow = "rgb(181, 234, 215) 0px 1px";
+	}
+
+	//Test amount input
+	if(amount.value == 0){
+		amount.style.boxShadow = "rgb(255,105,97) 0px 1px";
+		isEmpty = true;
+	}
+	else{
+		amount.style.boxShadow = "rgb(181, 234, 215) 0px 1px";
+	}
+
+	if(isEmpty){
+		alertString = alertString.concat(" ", "Please fill out all required fields.");
+		isInvalid = true;
+	}
+	if(isInvalid){
+		alert(alertString);
+	}
+	return isInvalid;
+}
+
